08ac5d8f0df598a89ad266982c2ffa3d8b33e5c8
Vu Huy Dong <huydongvu0105@gmail.com>
2024-04-20 01:06:16 +0700

    update 1h06 20/4/2024

--- LTNC_project/testGame/main.cpp
+++ LTNC_project/testGame/main.cpp
@@ -4,21 +4,27 @@ using namespace std;
 
 const string wordList[] = {"angle", "ant"};
 const int wordCount = sizeof(wordList) / sizeof(string);
-const int maxScore = 1000;
+const int maxScore = 100;
 
 string chooseWord();
+bool playGame(int userScore, string systemWord);
 string inputFromUser();
 void wordSize(int systemWord_length);
+int currentScore(int userScore, bool flag);
 bool checkWord(string systemWord, string userWord);
+char playAgain();
 
 int main(){
-    srand(time(0));
-    string systemWord = chooseWord();
-    wordSize(systemWord.length());
-    string userWord = inputFromUser();
-    int yourScore = maxScore;
-    if(checkWord(systemWord, userWord)){
-
+    while (1){
+        srand(time(0));
+        string systemWord = chooseWord();
+        wordSize(systemWord.length());
+        int userScore = maxScore;
+        playGame(userScore, systemWord);
+        char option = playAgain();
+        if(option == 'n' || option == 'N'){
+            break;
+        }
     }
     return 0;
 }
@@ -29,15 +35,42 @@ string chooseWord()
     return wordList[randomIndex];
 }
 
+bool playGame(int userScore, string systemWord){
+    string userWord;
+    bool flag = false;
+    while(1){
+        if(userScore <= 0){
+            cout << "Your score less than 0! You lose."<<endl;
+            return false;
+        }
+        userWord = inputFromUser();
+        if (checkWord(systemWord, userWord)){
+            flag = true;
+            userScore = currentScore(userScore, flag);
+            return true;
+        } else {
+            userScore = currentScore(userScore, flag);
+        }
+    }
+}
+
 string inputFromUser(){
     string input;
-    cout<< "Please answer: " << endl;
+    cout<< "Please type your answer: " << endl;
     cin>>input;
     return input;
 }
 
 void wordSize(int systemWord_length){
     cout << "There are " << systemWord_length << " characters" <<endl;
+}
+
+int currentScore(int userScore, bool flag){
+    if(flag == false){
+        userScore-=10;
+    }
+    cout <<"Your score is: " << userScore << "." <<endl;
+    return userScore;
 }
 
 bool checkWord(string systemWord, string userWord){
@@ -47,9 +80,17 @@ bool checkWord(string systemWord, string userWord){
     } else if (userWord.length() < systemWord.length()){
         cout<<"Error: More than "<<systemWord.length()<<" characters !" <<endl;
         return false;
-    } else if (userWord == systemWord){
+    }else if (userWord.length() == systemWord.length() && userWord == systemWord){
         cout << "Exactly !" << endl;
         return true;
     }
+    cout<<"Not Correct! Please try again."<<endl;
     return false;
+}
+
+char playAgain(){
+    char option;
+    cout << "Do you want to play again? Press ANY KEYS to play again or N to quit."<<endl;
+    cin >> option;
+    return option;
 }
